ad57cbe2f0e06625d742ce8ec36af3fed5e60461
aneff <aneff701@gmail.com>
2025-10-09 17:54:04 -0400

    install basic deps

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// src/App.jsx
+import React from "react";
+
+function App() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontSize: "2rem",
+      fontWeight: "bold",
+      backgroundColor: "#f5f5f5",
+      color: "#333",
+    }}>
+      This is our AI summarizer website
+    </div>
+  );
+}
+
+export default App;
