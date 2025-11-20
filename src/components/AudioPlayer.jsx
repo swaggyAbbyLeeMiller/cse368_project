9966f249776cc951e64eb007071d572bdbbbb69c
Fariha Farhin <farihafa@buffalo.edu>
2025-11-20 17:27:11 -0500

    Quick fix to audio player's css

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -2,9 +2,11 @@ import "../App.css"
 export default function AudioPlayer({ audioUrl }) {
   if (!audioUrl) return null;
   return (
+    <div className="lblock">
     <div>
       <h3>Listen:</h3>
       <audio controls src={audioUrl}></audio>
+    </div>
     </div>
   );
 }
